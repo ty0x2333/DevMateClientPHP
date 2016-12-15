72fb3f9e4107b65cff3c56af1d39c97904ed7fa3
luckytianyiyan <luckytianyiyan@gmail.com>
2016-12-16 01:06:33 +0800

    A DevMateConstants

---
+++ src/DevMateConstants.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luckytianyiyan
+ * Date: 16/12/16
+ * Time: 上午1:01
+ */
+
+namespace Ty\DevMate;
+
+class DevMateConstants
+{
+    const BASE_URL = "https://public-api.devmate.com";
+}
