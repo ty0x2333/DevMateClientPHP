7cacbd415a7199a15f23ee343b1c0a9e1b916269
luckytianyiyan <luckytianyiyan@gmail.com>
2016-12-17 01:35:19 +0800

    M ClientTest add testCreateLicense

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -13,11 +13,24 @@ use Ty\DevMate\Client;
 
 class ClientTest extends \PHPUnit_Framework_TestCase
 {
+    const API_KEY = "1e8b252e67adeecc9b94fe139ca80240294ab56209c6d89d1f7437c28dc37713";
+    
     public function testFetchCustomers()
     {
-        $client = new Client('1e8b252e67adeecc9b94fe139ca80240294ab56209c6d89d1f7437c28dc37713');
+        $client = new Client(ClientTest::API_KEY);
         list($status, $customers) = $client->fetch_customers();
         $this->assertEquals(200, $status);
         $this->assertGreaterThan(0, count($customers));
+    }
+    
+    public function testCreateLicense()
+    {
+        $client = new Client(ClientTest::API_KEY);
+        $customer_id = 2;
+        $license_type_id = 1;
+        list($status, $license) = $client->create_license($customer_id, $license_type_id);
+        // maybe 200 or 201
+        $this->assertGreaterThanOrEqual(200, $status);
+        $this->assertNotNull($license);
     }
 }
