f3bb22c0c7a587f73e97b44fbdb36683103d0383
luckytianyiyan <luckytianyiyan@gmail.com>
2016-12-16 19:56:40 +0800

    M Client add fetch_customers

--- src/Client.php
+++ src/Client.php
@@ -17,4 +17,57 @@ class Client
     {
         $this->api_key = $api_key;
     }
+    
+    public function getAuthHeader()
+    {
+        if (!empty($this->api_key)) {
+            return array(
+                'Authorization: Token ' . $this->api_key
+            );
+        }
+    }
+
+    public function fetch_customers(CustomerFiltration $filtration = NULL, $limit = NULL, $offset = NULL, $licenses = NULL)
+    {
+        $data = array(
+            'limit' => $limit,
+            'offset' => $offset,
+            'with' => $licenses,
+        );
+
+        if ($filtration) {
+            $data = array_merge($data, $filtration->body());
+        }
+        
+        $query_string = http_build_query($data);
+        $url = rtrim(trim(DevMateConstants::BASE_URL), '/') . "/v2/customers";
+        if (!empty($query_string)) {
+            $url = $url . '?' . $query_string;
+        }
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->getAuthHeader());
+        
+        $response = curl_exec($ch);
+        $httpStatus = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        // Get Request and Response Headers
+        $requestHeaders = curl_getinfo($ch, CURLINFO_HEADER_OUT);
+        // Using alternative solution to CURLINFO_HEADER_SIZE as it throws invalid number when called using PROXY.
+        if (function_exists('mb_strlen')) {
+            $responseHeaderSize = mb_strlen($response, '8bit') - curl_getinfo($ch, CURLINFO_SIZE_DOWNLOAD);
+            $responseHeaders = mb_substr($response, 0, $responseHeaderSize, '8bit');
+            $response = mb_substr($response, $responseHeaderSize, mb_strlen($response), '8bit');
+        } else {
+            $responseHeaderSize = strlen($response) - curl_getinfo($ch, CURLINFO_SIZE_DOWNLOAD);
+            $responseHeaders = substr($response, 0, $responseHeaderSize);
+            $response = substr($response, $responseHeaderSize);
+        }
+
+        curl_close($ch);
+        return array($httpStatus, $response);
+    }
+
 }
